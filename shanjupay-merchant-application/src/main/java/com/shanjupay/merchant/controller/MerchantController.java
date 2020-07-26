823ef0b922590b5093bca00c47fffc6557bff9a7
CharleCheng <chengzhikang205@163.com>
2020-07-26 10:44:37 +0800

    day3,验证码注册

--- shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/controller/MerchantController.java
+++ shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/controller/MerchantController.java
@@ -2,13 +2,13 @@ package com.shanjupay.merchant.controller;
 
 import com.shanjupay.merchant.api.MerchantService;
 import com.shanjupay.merchant.api.dto.MerchantDTO;
+import com.shanjupay.merchant.service.SmsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Administrator
@@ -16,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @Api(value="商户平台应用接口",tags = "商户平台应用接口tag")
+@Slf4j
 public class MerchantController {
 
-    @org.apache.dubbo.config.annotation.Reference
+    @org.apache.dubbo.config.annotation.Reference  //注入的远程调用的接口
     MerchantService merchantService;
+
+    @Autowired  //注入本地的bean
+    SmsService smsService;
 
     @ApiOperation(value="根据id查询商户信息")
     @GetMapping("/merchants/{id}")
@@ -27,6 +31,15 @@ public class MerchantController {
 
         MerchantDTO merchantDTO = merchantService.queryMerchantById(id);
         return merchantDTO;
+    }
+
+    @ApiOperation("获取手机验证码")
+    @GetMapping("/sms")
+    @ApiImplicitParam(value = "手机号",name = "phone",required = true,dataType = "string",paramType = "query")
+    public String getSMSCode(@RequestParam("phone") String phone){
+        log.info("向手机号:{}发送验证码: ", phone);
+        //向验证码服务请求发送验证码
+        return smsService.sendMsg(phone);
     }
 
     @ApiOperation("测试")
